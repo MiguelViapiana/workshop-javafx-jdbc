d52e219ff9b0c361b2731150a214895499efdf84
MiguelViapiana <migvjung@gmial.com>
2024-10-06 15:43:45 -0300

    Observer design pattern to update tableview

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Utils;
 import javafx.collections.FXCollections;
@@ -26,7 +27,7 @@ import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
 
-public class DepartmentListController implements Initializable{
+public class DepartmentListController implements Initializable, DataChangeListener{
 
 	private DepartmentService service;
 	
@@ -57,7 +58,6 @@ public class DepartmentListController implements Initializable{
 	
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-		
 		initializeNodes();
 	}
 
@@ -66,8 +66,7 @@ public class DepartmentListController implements Initializable{
 		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
 		
 		Stage stage =(Stage) Main.getMainScene().getWindow();
-		tableViewDepartments.prefHeightProperty().bind(stage.heightProperty());
-		
+		tableViewDepartments.prefHeightProperty().bind(stage.heightProperty());	
 	}
 	
 	public void updateTableView() {
@@ -87,10 +86,10 @@ public class DepartmentListController implements Initializable{
 			DepartmentFormController controller = loader.getController();
 			controller.setDepartment(obj);
 			controller.updateFormData();
+			controller.subscribeDataChangeListener(this);
 			controller.setDepartmentService(new DepartmentService());
 			
 			Stage dialogStage = new Stage();
-			
 			dialogStage.setTitle("Enter Department data");
 			dialogStage.setScene(new Scene(pane));
 			dialogStage.setResizable(false);
@@ -101,5 +100,11 @@ public class DepartmentListController implements Initializable{
 		catch(IOException e) {
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
+	}
+
+	@Override
+	public void onDataChanged() {
+		updateTableView();
+		
 	}
 }
